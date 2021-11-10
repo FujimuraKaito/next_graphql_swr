b709c37115d0a1d70a0539f591bc5e3f4f38081f
FujimuraKaito <kaito.bakuman@gmail.com>
2021-11-11 00:26:07 +0900

    ✨ add graphql type def

---
+++ src/apollo/resolver.ts
@@ -0,0 +1,20 @@
+import { Config } from 'apollo-server-micro'
+
+const SAMPLE_DB = {
+  articles: [
+    { id: 1, title: 'foo', content: 'fooooo' },
+    { id: 2, title: 'bar', content: 'baaaar' },
+  ],
+}
+
+const getArticleResolver = (_: any, { id }: { id: number }) =>
+  SAMPLE_DB.articles?.filter((article) => article.id === id)[0] ?? []
+
+const getArticles = () => SAMPLE_DB.articles
+
+export const resolvers: Config['resolvers'] = {
+  Query: {
+    getArticle: getArticleResolver,
+    getArticles: getArticles,
+  },
+}
